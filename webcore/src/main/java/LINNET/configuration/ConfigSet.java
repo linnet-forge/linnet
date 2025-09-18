d8031fe0db358bf603b7a40589adcdf195fb8833
миша <rulko.mkes@gmail.com>
2025-09-18 21:13:34 +0300

    added fundamental classes + configuration

--- webcore/src/main/java/LINNET/configuration/ConfigSet.java
+++ webcore/src/main/java/LINNET/configuration/ConfigSet.java
@@ -1,32 +1,40 @@
 package LINNET.configuration;
 
+import org.hibernate.cfg.Environment;
 import org.json.JSONObject;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class ConfigSet {
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReaderENV = new BufferedReader(new FileReader("D:/linnet/webcore/src/main/java/LINNET/configuration/.env"));
+        BufferedWriter bufferedWriterPROP = new BufferedWriter(
+                new FileWriter("src/main/resources/application.properties"));
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter("application.properties"));
-        Scanner scanner = new Scanner(System.in);
+        LinkedList<String> Name = new LinkedList<>(Arrays.asList(
+                "spring.application.name",
+                "spring.datasource.url",
+                "spring.datasource.username",
+                "spring.datasource.password",
+                "server.servlet.session.timeout",
+                "server.servlet.session.cookie.name",
+                "server.servlet.session.cookie.http-only"
+        ));
 
-        String jsonString = """
-                {"config":
-                    {
-                    "name" : "webcore",
-                    "url" : "jdbc:postgresql://localhost:5432/testdb",
-                    "username" : "postgres",
-                    "password" : "0011",
-                    "timeout" :"1800",
-                    "cookie.name" : "MYSESSIONID",
-                    "session.cookie.http-only" : "true"
-                    }
-                }
-                """;
-        JSONObject jsonObject = new JSONObject(jsonString);
-        System.out.println(jsonObject);
+        LinkedList<String> Key = new LinkedList<>();
+        String line;
+        while ((line=bufferedReaderENV.readLine())!=null){
+            Key.add(line);
+        }
+        for (int i = 0; i < Key.size(); i++){
+            bufferedWriterPROP.write(Name.get(i) + "=" + Key.get(i));
+            bufferedWriterPROP.newLine();
+        }
+
+        bufferedReaderENV.close();
+        bufferedWriterPROP.close();
     }
 }
