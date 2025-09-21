e06a0a904b7184241b5f88ba9166d072066acd5a
миша <rulko.mkes@gmail.com>
2025-09-21 15:41:06 +0300

    frontend

--- webcore/src/main/java/LINNET/webcore/WebcoreApplication.java
+++ webcore/src/main/java/LINNET/webcore/WebcoreApplication.java
@@ -10,23 +10,27 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 public class WebcoreApplication {
-	private static final Logger logger = Logger.getLogger(WebcoreApplication.class.getName());
-
-	public static void main(String[] args) throws IOException {
-		ConfigSet configSet = new ConfigSet();
-		WebcoreApplication app = new WebcoreApplication();
-		configSet.setCfg();
-		try {
-			SpringApplication.run(WebcoreApplication.class, args);
-		} catch (Exception e){
-			logger.warning("Error with booting module " + e);
-		}
-
+	public static void main(String[] args) {
+		SpringApplication.run(WebcoreApplication.class,args);
 	}
+//	private static final Logger logger = Logger.getLogger(WebcoreApplication.class.getName());
+//
+//	public static void main(String[] args) throws IOException {
+//		ConfigSet configSet = new ConfigSet();
+//		WebcoreApplication app = new WebcoreApplication();
+//		configSet.setCfg();
+//		try {
+//			SpringApplication.run(WebcoreApplication.class, args);
+//		} catch (Exception e){
+//			logger.warning("Error with booting module " + e);
+//		}
+//
+//	}
+//
+//	@PreDestroy
+//	public void shutDown() throws IOException {
+//		ConfigSet configSet = new ConfigSet();
+//		configSet.clearCfg();
+//	}
 
-	@PreDestroy
-	public void shutDown() throws IOException {
-		ConfigSet configSet = new ConfigSet();
-		configSet.clearCfg();
-	}
 }
